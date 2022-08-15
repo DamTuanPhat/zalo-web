778bb31413b72a7a34177c069582000b5e6a082f
DamTuanPhat <damtuanphat@gmail.com>
2022-08-16 00:26:37 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              './app.responsive.css']
 })
 export class AppComponent {
   title = 'zalo-web';
